3e89f2ca0d05d4520be7d131c2ab2fd1b2006c55
Anikushin Roman <anikushin.roma@gmail.com>
2023-04-17 13:25:10 +0700

    Исправления бага с авторизацией

--- messenger-boot-common/src/main/java/ru/greenpix/messenger/common/configuration/WebSecurityConfiguration.java
+++ messenger-boot-common/src/main/java/ru/greenpix/messenger/common/configuration/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package ru.greenpix.messenger.common.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,8 +24,11 @@ public class WebSecurityConfiguration {
     /**
      * Bean настройки spring web security.
      * В настройке учитываются авторизации по JWT и API-KEY.
+     *
+     * Аннотация {@link Order} нужна для корректной работы.
      */
     @Bean
+    @Order(2)
     public SecurityFilterChain securityWebFilterChain(
             HttpSecurity http,
             JwtAuthenticationConverter jwtConverter,
